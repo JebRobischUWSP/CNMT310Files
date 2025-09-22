84f1a9eb5742259ae066476662481cb82197a0e3
Robisch <jrobi774@uwsp.edu>
2025-09-22 09:40:29 -0500

    php added

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+$title = "Very very cool site";
+print "<!doctype html>\n";
+print "<html lang=\"en\">\n";
+	print "<head>\n";
+		print "<title>". $title ."</title>\n";
+	print "</head>\n";
+	print "<body>\n";
+		print "<h1> HELLO WORLD!!!1! </h1>\n";
+	print "</body>\n";
+print "</html>\n";
+?>
