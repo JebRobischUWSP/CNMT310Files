fdc6704d0ce87399aacf275ad44659acdc72342d
Robisch <jrobi774@uwsp.edu>
2025-09-24 08:55:46 -0500

    New PHP formatting added

--- index.php
+++ index.php
@@ -1,12 +1,23 @@
 <?php
 $title = "Very very cool site";
+$name = "Jeb";
 print "<!doctype html>\n";
 print "<html lang=\"en\">\n";
-	print "<head>\n";
-		print "<title>". $title ."</title>\n";
-	print "</head>\n";
-	print "<body>\n";
-		print "<h1> HELLO WORLD!!!1! </h1>\n";
-	print "</body>\n";
+print "<head>\n";
+print "<title>". $title ."</title>\n";
+print "</head>\n";
+print "<body>\n";
+print "<h1> HELLO WORLD!!!1! </h1>\n";
+print "<p>Hey ".$name.", today is ".date("j-n-Y")."</p>\n";
+print "<p>The server's variable list is as follows: ";
+foreach ($_SERVER as $k => $v) {
+	if ($k === "SERVER_SIGNATURE") {
+		print "<br>Key [".$k."] has been ommitted due to breaking the html :(";
+	} else {
+		print "<br>Key [".$k."] has value '".$v."'";
+	}
+}
+print"</p>\n";
+print "</body>\n";
 print "</html>\n";
 ?>
